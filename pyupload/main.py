c02bb4fb26581db3c313c052f158acc56a7dfc7b
mdjhacker <mdjhacker@126.com>
2022-02-09 21:54:07 +0800

    1

--- pyupload/main.py
+++ pyupload/main.py
@@ -1,5 +1,8 @@
+import time
+import shelve
 from pyupload.uploader import *
-import click
+from PicImageSearch import SauceNAO
+import os
 
 uploader_classes = {
     "catbox": CatboxUploader,
@@ -9,15 +12,92 @@ uploader_classes = {
 }
 
 
-@click.command()
-@click.option('--host', default='catbox', help='catbox/mixtape/uguu/fileio')
-@click.argument('name')
-def upload(host, name):
-    uploader_class = uploader_classes[host]
-    uploader_instance = uploader_class(name)
-    result = uploader_instance.execute()
-    print("Your link : {}".format(result))
+def upload():
+    rootdir = '.\data'
+    data_list = os.listdir(rootdir)  # 列出文件夹下所有的目录与文件
+    for i in range(0, len(data_list)):
+        # path = os.path.join(rootdir, data_list[i])
+        new_path = 'data/' + data_list[i]
+        print("文件名称 : " + data_list[i])
+
+        # catbox uploader
+        uploader_class = uploader_classes["catbox"]
+        uploader_instance = uploader_class(new_path)
+        result = uploader_instance.execute()  # catbox link
+        print("OK")
+        # print("Your link : {}".format(result))
+
+        saucenao = SauceNAO(api_key='bb3d05cfd3779f1e3b8c6131c9c1b57979dc7994')  # api_key location
+        res = saucenao.search(result)
+
+        print("24h访问额度剩余 : " + str(res.long_remaining))  # 每天访问额度剩余
+        print("30s访问额度剩余 : " + str(res.short_remaining))  # 每30秒访问额度剩余
+        print("略缩图地址 : " + res.raw[0].thumbnail)  # 略缩图地址
+        print("相似度 : " + str(res.raw[0].similarity) + "%")  # 相似度
+        print("相似作品标题 : " + res.raw[0].title)  # 相似的标题
+        print("相似作品作者 : " + res.raw[0].author)  # 作者
+        # print("相似作品链接 : " + res.raw[0].url)  # 相似链接地址
+        # print("搜索数据库深度 : " + res.search_depth)  # 搜索数据库个数
+        print("\n")
+        time.sleep(7)
+
+
+def local_save():
+    s = shelve.open('test_shelf.db')  #
+    try:
+        s['kk'] = {'int': 10, 'float': 9.5, 'String': 'Sample data'}
+        s['MM'] = [1, 2, 3]
+    finally:
+        s.close()
+
+    try:
+        s = shelve.open('test_shelf.db')
+        value = s['kk']
+        print(value)
+    finally:
+        s.close()
+
+    # s = shelve.open('test_shelf.db', flag='w', writeback=True)
+    # try:
+    #     # 增加
+    #     s['QQQ'] = 2333
+    #     # 删除
+    #     del s['MM']
+    #     # 修改
+    #     s['kk'] = {'String': 'day day up'}
+    # finally:
+    #     s.close()
+    #
+    # s = shelve.open('test_shelf.db')
+    # try:
+    #     # 方法一：
+    #     for item in s.items():
+    #         print('键[{}] = 值[{}]'.format(item[0], s[item[0]]))
+    #     # 方法二：
+    #     for key, value in s.items():
+    #         print(key, value)
+    # finally:
+    #     s.close()
 
 
 if __name__ == "__main__":
-    upload()
+    print('''\033[36m
+     __  .___  ___.      ___       _______  _______ 
+    |  | |   \/   |     /   \     /  _____||   ____|
+    |  | |  \  /  |    /  ^  \   |  |  __  |  |__   
+    |  | |  |\/|  |   /  /_\  \  |  | |_ | |   __|  
+    |  | |  |  |  |  /  _____  \ |  |__| | |  |____ 
+    |__| |__|  |__| /__/     \__\ \______| |_______|
+      ______  __    __   _______   ______  __  ___  _______ .______      
+     /      ||  |  |  | |   ____| /      ||  |/  / |   ____||   _  \     
+    |  ,----'|  |__|  | |  |__   |  ,----'|  '  /  |  |__   |  |_)  |    
+    |  |     |   __   | |   __|  |  |     |    <   |   __|  |      /     
+    |  `----.|  |  |  | |  |____ |  `----.|  .  \  |  |____ |  |\  \----.
+     \______||__|  |__| |_______| \______||__|\__\ |_______|| _| `._____|
+                                               Author:HuangYu Version:1.0
+    \033[0m ''')
+
+    # api_key = input('请输入Api_key:')
+
+    # upload()
+    local_save()
